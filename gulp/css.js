6d26df0505e99f169817b8ae54b37d612552e73d
yisraelgrimes <yisrael@tubemedia.co>
2017-03-03 17:47:44 -0600

    v1.0.3 Removed clean:css task from gulp

--- gulp/css.js
+++ gulp/css.js
@@ -14,9 +14,3 @@ gulp.task( 'test:css', function() {
 	return gulp.src( pathy.css.all )
 		.pipe( parker() )
 } );
-
-
-// Deletes generated css files
-gulp.task( 'clean:css', shell.task( [
-	'rm -rf ' + pathy.css.all
-] ) );
