4f508b7460c86b9e20b890af2964cab3971190a6
yisraelgrimes <yisrael@tubemedia.co>
2017-04-08 05:14:44 -0500

    Hotfix- gulp-sourcemaps and removed stream error with sass-tabify

--- gulp/sass.js
+++ gulp/sass.js
@@ -8,6 +8,7 @@ var sass         = require( 'gulp-sass'     );  // Compiles sass into css
 var browserSync  = require( 'browser-sync'  );  // Live browser reloading
 var autoprefixer = require('gulp-autoprefixer' ); // Browser Prefixer for Sass
 var sassLint     = require( 'gulp-sass-lint');  // Lint Sass w/ '.sass-lint.yml'
+var sourcemaps   = require( 'gulp-sourcemaps'); // Creates CSS Sourcemaps
 var tabify       = require( 'gulp-tabify'   );  // Converts spaces to tabs
 var runSequence  = require( 'run-sequence'  );  // Sets order for tasks to run
 
@@ -43,9 +44,11 @@ gulp.task( 'lint-sass', function () {
 // Compiles, formats, prefixes, and lints sass
 gulp.task( 'sass-compile', function() {
 	return gulp.src( pathy.sass.all )
-		.pipe( tabify( 2, true ) )
-		.pipe( gulp.dest( pathy.sass.dir ) )
+		// .pipe( tabify( 2, true ) )
+		// .pipe( gulp.dest( pathy.sass.dir ) )
+		.pipe( sourcemaps.init() )
 		.pipe( sass( optys.sass.output ).on( 'error', sass.logError ) )
+		.pipe( sourcemaps.write() )
 		.pipe( autoprefixer( optys.sass.prefixer ) )
 		.pipe( gulp.dest( pathy.sass.dest ) )
 		.pipe( browserSync.reload( { stream: true } ) )
